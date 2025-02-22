1616eaf90c11df4939698a20d97a552ef13ebcdd
Ferv0r2 <amlk31255@gmail.com>
2025-02-22 14:24:36 +0900

    [Learning] Exercise - Collatz Sequence answer

--- 02_control_flow_basics/example_collatz_sequence/src/main.rs
+++ 02_control_flow_basics/example_collatz_sequence/src/main.rs
@@ -35,6 +35,27 @@ fn collatz_sequence_fp(n: i32) -> usize {
     -1 // 시작 값 n을 포함하기에 -1 해야 한다.
 }
 
+/**
+ * Comprehensive Rust - Solution
+ * 
+ * 한 줄 if문 형태로 진행
+ * 테스트를 포함한 코드, #[test] 어노테이션 반영
+ * -> `cargo test` 시 자동 실행
+ */
+
+ fn collatz_length(mut n: i32) -> u32 {
+    let mut len = 1;
+    while n > 1 {
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+        len += 1;
+    }
+    len
+}
+
+#[test]
+fn test_collatz_length() {
+    assert_eq!(collatz_length(11), 15);
+}
 
 fn main() {
     let n = 100;
